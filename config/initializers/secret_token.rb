7434b5d7b93bd0e7437c5bd5b5134ba2d7bae4c2
Tom Dooner <tom@causes.com>
2013-03-14 22:34:35 -0400

    Empty Rails App

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GimmeIceCream::Application.config.secret_token = 'daff8164dd28b7b8f4182ad84d05af79a8d1a58a83f22388c5ff6d15cc67cf33dff93e6c518a7e697e45e74f7a18d6922fe4586452433f123e2496785b1ddc87'
